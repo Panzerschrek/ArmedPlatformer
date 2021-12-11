0062da9dc778cb023f8ae463d1a175c7e94297c1
Panzerschrek <AntiPanzerschrek@yandex.ru>
2021-12-11 12:35:34 +0300

    Sozdajom "WIN32" priloženije pod "Windows" - bez okna konsoli.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include "Log.hpp"
 #include "Host.hpp"
+#include <SDL.h>
 
 namespace Armed
 {
 
-extern "C" int main()
+extern "C" int main(const int argc, const char* argv[])
 {
+	ARMED_UNUSED(argc);
+	ARMED_UNUSED(argv);
+
 	Host host;
 	while(!host.Loop()){}
 	return 0;
